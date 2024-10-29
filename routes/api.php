71c434c758a71f930b09874c576c34b6e90cf0fd
Omotola Awokunle <omotolaawokunle@gmail.com>
2024-10-29 08:54:05 +0100

    Addition: Add API endpoints to store transactions and get balance

---
+++ routes/api.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/transactions', [\App\Http\Controllers\TransactionController::class, 'store']);
+    Route::get('/balance', [\App\Http\Controllers\GetBalance::class]);
+});
