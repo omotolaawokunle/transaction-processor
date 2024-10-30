7e2e3728f57744a1b1616b59434bd8f009b55d16
Omotola Awokunle <omotolaawokunle@gmail.com>
2024-10-30 08:23:01 +0100

    Fix: Fix balance route bug

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/transactions', [\App\Http\Controllers\TransactionController::class, 'store']);
-    Route::get('/balance', [\App\Http\Controllers\GetBalance::class]);
+    Route::get('/balance', \App\Http\Controllers\GetBalance::class);
 });
